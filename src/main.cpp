9fecf38202259d7c438502ad3bdad90bc11e89b7
Juan David Cruz Gomez <juandavidcruz@gmail.com>
2017-11-12 23:48:13 +0100

    json test

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 #include "Country.hpp"
 #include "HttpWrapper.h"
 
+#include "json.hpp"
+
+using json = nlohmann::json;
+
 int main(int argc, char **argv) {
     Country* country = new Country("Colombia", 4.4, 72, "CO");
     std::cout << "Hello, world from " << country->getName() << std::endl;
     delete country;
-    getHttpReq("http://www.google.com");
+    std::string page = getHttpReq("https://nordvpn.com/api/server"); //https://nordvpn.com/api/server/stats
+    json j = json::parse(page.c_str());
+    std::cout << j.dump(4) << std::endl;
     return 0;
 }
